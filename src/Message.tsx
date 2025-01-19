eda9ada4cc9ab01de907c7600d99100bc035a92c
NomaTyx <jjziebart99@gmail.com>
2025-01-19 00:01:07 -0800

    added message file and changed app to reflect the contents of message

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+//THIS FILE IS FROM TUTORIAL AND CAN BE REMOVED.
+
+function Message() {
+    //JSX: Javascript XML (whatever that means aaaaaaaaaa)
+    const name = 'penis';
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
